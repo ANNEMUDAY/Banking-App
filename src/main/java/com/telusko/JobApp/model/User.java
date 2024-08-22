f628ddaff39d740a3eb81c332e9ace3b922d7046
ANNEMUDAY <udayannem197@gmail.com>
2024-08-22 22:14:41 +0530

    Changes made to User

--- src/main/java/com/telusko/JobApp/model/User.java
+++ src/main/java/com/telusko/JobApp/model/User.java
@@ -22,16 +22,13 @@ public class User {
     public String getUsername() {
         return username;
     }
-    public String setUsername(){
-        return username;
-    }
+//    public String setUsername(){
+//        return username;
+//    }
     public void setPassword(String password) {
         this.password = password;
     }
 
 
-//    public String setPassword(String encode) {
-//        return encode;
-//    }
 
 }
